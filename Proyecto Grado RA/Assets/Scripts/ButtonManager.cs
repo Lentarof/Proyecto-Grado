72307bb6c176eacccedd0ffd53bcf019c1fd953b
Ray Silva <onikageriki@gmail.com>
2021-10-17 20:32:25 -0400

    DOTWeen Menu Scene Game Scene  UI Gallery
    Se realizaron diferentes cambios con referente al UI de la galeria, se agregaron escenas que pueden o no ser utilizadas sujeto a cambios

--- Proyecto Grado RA/Assets/Scripts/ButtonManager.cs
+++ Proyecto Grado RA/Assets/Scripts/ButtonManager.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 public class ButtonManager : MonoBehaviour
 {
     private Button btn;
+
     public GameObject plant;
     // Start is called before the first frame update
     void Start()
@@ -16,7 +18,18 @@ public class ButtonManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //si este boton a entrado al raycast lo vamos a escalar
+        if (UIManager.Instance.OnEntered(gameObject))
+        {
+                                      //el primer valor es cuanto escalar el segundo es el valor de que tan rapido o lento quieres la animacion
+            transform.DOScale(Vector3.one * 2, 0.3f);
+            //transform.localScale = Vector3.one * 2;
+        }
+        else
+        {
+            transform.DOScale(Vector3.one, 0.3f);
+            //transform.localScale = Vector3.one;
+        }
     }
 
     void SelectObject() 
